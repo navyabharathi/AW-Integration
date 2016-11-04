1a3eba01542bb92fddb193cf21b53166626b06f1
navya <navya@1.1.4.117>
2016-11-04 10:13:55 +0530

    first commit

---
+++ src/com/integration/github/IntegrationsServlet.java
@@ -0,0 +1,12 @@
+package com.integration.github;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class IntegrationsServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
